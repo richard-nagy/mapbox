f229f8a84c55c31bdb5b492da65ff1a8bf6e9428
Richárd Nagy <nagy.richard@thisisdone.com>
2022-10-24 14:17:43 +0200

    Add movable map

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-function App() {
-    return <div>MapBox</div>;
-}
+import React, { useState } from "react";
+import ReactMapGL from "react-map-gl";
 
-export default App;
+export default function App() {
+    const [viewport, setViewport] = useState({
+        width: "100vw",
+        height: "100vh",
+        latitude: 47.1028,
+        longitude: 17.9093,
+        zoom: 10,
+        pitch: 0,
+        bearing: 0,
+    });
+
+    return (
+        <div className="App">
+            <ReactMapGL
+                {...viewport}
+                mapStyle="mapbox://styles/rafilos556/ckhrp0auk0ol119s02qvctvh4"
+                mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+                onViewportChange={(nextViewport) => setViewport(nextViewport)}
+            ></ReactMapGL>
+        </div>
+    );
+}
